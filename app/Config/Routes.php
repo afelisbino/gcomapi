dfc64597a8aac750ea648c9bd512315a9a9e5991
Adriano Silva <adrsilva613@gmail.com>
2021-03-10 15:03:12 -0300

    Criação de novas rotas de requisições

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -94,6 +94,7 @@ $routes->group('api', ['namespace' => 'App\Api\v1'], function($routes){
 		$routes->group('view', function($routes){
 			$routes->get('today', 'Venda::totalSaleToday');
 		});
+		$routes->put('pay', 'Venda::payPayments');
 	});
 
 	$routes->group('cash', function($routes){
@@ -103,6 +104,13 @@ $routes->group('api', ['namespace' => 'App\Api\v1'], function($routes){
 		$routes->group('view', function($routes){
 			$routes->get('status', 'Caixa::statusCaixa');
 		});
+		$routes->post('input', 'Caixa::inputCash');
+		$routes->post('output', 'Caixa::outputCash');
+	});
+
+	$routes->group('client', function($routes){
+		$routes->get('list', 'Cliente::listClient');
+		$routes->post('new', 'Cliente::newClient');
 	});
 });
 
